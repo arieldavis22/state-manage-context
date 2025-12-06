6513683723e13addd6c17232c6e968c0f78d78e3
Ariel <aadavisbusiness@gmail.com>
2025-12-05 19:15:32 -0600

    feat: link context to state

--- src/App.jsx
+++ src/App.jsx
@@ -67,9 +67,14 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
     <>
-      <CartContext>
+      <CartContext value={ctxValue}>
         <Header
           cart={shoppingCart}
           onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
